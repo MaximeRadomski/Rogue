0f44f1d577f55c5ea49f68b128d39cf82aaca7e2
Maxime Radomski <maxime.radomski@gmail.com>
2019-10-25 14:09:37 +0200

    First Creation

---
+++ Assets/Scripts/Generics/ButtonBhv.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBhv : MonoBehaviour
+{
+    public void BeginAction()
+    {
+    }
+
+    public void DoAction()
+    {
+    }
+
+    public void EndAction()
+    {
+    }
+}
---
+++ Assets/Scripts/Generics/InputControlerBhv.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputControlerBhv : MonoBehaviour
+{
+    private Vector3 _touchPosWorld;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                _touchPosWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+                Vector2 touchPosWorld2D = new Vector2(_touchPosWorld.x, _touchPosWorld.y);
+                RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+                if (hitInformation.collider != null)
+                {
+                    GameObject touchedObject = hitInformation.transform.gameObject;
+                    if (touchedObject.tag == "Button")
+                    {
+                        if (Input.GetTouch(i).phase == TouchPhase.Began)
+                            touchedObject.GetComponent<ButtonBhv>().BeginAction();
+                        else if (Input.GetTouch(i).phase == TouchPhase.Ended)
+                            touchedObject.GetComponent<ButtonBhv>().EndAction();
+                        else
+                            touchedObject.GetComponent<ButtonBhv>().DoAction();
+                    }
+                }
+            }
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            _touchPosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 touchPosWorld2D = new Vector2(_touchPosWorld.x, _touchPosWorld.y);
+            RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+            if (hitInformation.collider != null)
+            {
+                GameObject touchedObject = hitInformation.transform.gameObject;
+                if (touchedObject.tag == "Button")
+                {
+                    touchedObject.GetComponent<ButtonBhv>().BeginAction();
+                }
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            _touchPosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 touchPosWorld2D = new Vector2(_touchPosWorld.x, _touchPosWorld.y);
+            RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+            if (hitInformation.collider != null)
+            {
+                GameObject touchedObject = hitInformation.transform.gameObject;
+                if (touchedObject.tag == "Button")
+                {
+                    touchedObject.GetComponent<ButtonBhv>().EndAction();
+                }
+            }
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            _touchPosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 touchPosWorld2D = new Vector2(_touchPosWorld.x, _touchPosWorld.y);
+            RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+            if (hitInformation.collider != null)
+            {
+                GameObject touchedObject = hitInformation.transform.gameObject;
+                if (touchedObject.tag == "Button")
+                {
+                    touchedObject.GetComponent<ButtonBhv>().DoAction();
+                }
+            }
+        }
+        else
+            _touchPosWorld = new Vector3(-99,-99,-99);
+    }
+}
