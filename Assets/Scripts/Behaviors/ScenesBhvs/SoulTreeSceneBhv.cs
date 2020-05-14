81aabf79378c812e33221c64f6f80291baeab178
Maxime Radomski <maxime.radomski@gmail.com>
2020-05-14 10:35:44 +0200

    SoulTreeScene work

--- Assets/Scripts/Behaviors/ScenesBhvs/SoulTreeSceneBhv.cs
+++ Assets/Scripts/Behaviors/ScenesBhvs/SoulTreeSceneBhv.cs
@@ -49,6 +49,7 @@ public class SoulTreeSceneBhv : SceneBhv
     private void HandleTreeBranchDisplay(string stat)
     {
         int statLevel = (int)_soul.GetPropertyValue(stat + "_Level");
+        int statMax = (int)_soul.GetPropertyValue(stat + "_Max");
         int statId = (int)_soul.GetPropertyValue(stat + "_Id");
         int statPrice = (int)_soul.GetPropertyValue(stat + "_Price");
         GameObject treeBranch = GameObject.Find(stat);
@@ -60,7 +61,14 @@ public class SoulTreeSceneBhv : SceneBhv
         }
         treeBranch.transform.Find("Level").GetComponent<TMPro.TextMeshPro>().text = statLevel.ToString();
         var currentPrice = statPrice * (statLevel + 1);
-        if (_soul.Xp > currentPrice)
+        if (statLevel == statMax)
+        {
+            var priceObject = treeBranch.transform.Find("Price").GetComponent<TMPro.TextMeshPro>();
+            priceObject.enabled = true;
+            priceObject.text = "Max";
+            treeBranch.transform.Find("Add").GetComponent<SpriteRenderer>().enabled = false;
+        }
+        else if (_soul.Xp > currentPrice)
         {
             treeBranch.transform.Find("Price").GetComponent<TMPro.TextMeshPro>().enabled = false;
             treeBranch.transform.Find("Add").GetComponent<SpriteRenderer>().enabled = true;
